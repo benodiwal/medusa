0a4e6230d1839cfb55e0371c8fb86a84bedf99a3
benodiwal <s474996633@gmail.com>
2025-12-29 10:47:11 +0530

    chore: export lib modules

--- medusa/src/lib/index.ts
+++ medusa/src/lib/index.ts
@@ -1,8 +1,2 @@
-// API exports
-export * from './api';
-
-// Services exports
-export { WorkspaceService } from './services/workspaceService';
-
 // Utils exports
 export { FileSystemUtils } from './utils/filesystem';
